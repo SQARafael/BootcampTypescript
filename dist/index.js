cc012dde0ad4551eb89dccb71378d54593fef4eb
Rafael Chica <rafael.chica@sqasa.co>
2024-04-10 19:19:08 -0500

    proyecto inicial

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let firstName = 'Rafael';
+const age = 27;
+let numeros = [1, 2, 3];
+let numeros2 = [1, 2, 3];
+let array = [1, 'dos', true];
+console.log(`Hello world! My name is ${firstName} i'm ${age} years old`);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let firstName='Rafael';
+const age=27;
+
+let numeros: number[]=[1,2,3];
+let numeros2: Array<number>=[1,2,3];
+
+let array: any[]=[1, 'dos', true];
+
+console.log(`Hello world! My name is ${ firstName} i'm ${age} years old`)
